59730bcd3e975f3b19f0d6dbae76c579925a5286
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-12-11 13:25:42 +0100

    refactor(main.c): better usage

--- main.c
+++ main.c
@@ -7,24 +7,21 @@
 void usage() {
     qol_warn("Usage: <program> <param>\n");
     qol_warn("param:\n");
-    qol_warn("  maze   - Generate and display a maze\n");
-    qol_warn("  sort   - Generate and display a sort\n");
+    qol_warn("  maze   - Path finding Algorithms like Dijkstra.\n");
+    qol_warn("  sort   - Sorting Algorithms like Merge Sort.\n");
     qol_warn("  usage  - Show this usage information\n");
 }
 
 int main(int argc, char** argv) {
-    if (argc < 2) {
-        qol_error("Usage: {} <type>\n", argv[0]);
-        return 1;
-    }
+    const char* program = qol_shift(argc, argv); 
+    if (argc < 1) qol_error("Usage: %s <type>\n", program);
 
-    qol_shift(argc, argv); 
     const char* val = qol_shift(argc, argv);
 
     if (strcmp(val, "maze") == 0) maze();
     if (strcmp(val, "sort") == 0) sort();
     if (strcmp(val, "usage") == 0) usage();
-    else qol_warn("Unknown type: {}\n", val);
+    else qol_error("Unknown type: %s\n", val);
 
 
     return 0;
