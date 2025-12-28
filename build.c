1a9ff7b86b911b57674bbc13e5e9c3d60f792a08
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2025-12-28 13:02:58 +0100

    refactor(build.c): make build.c more dynamic

--- build.c
+++ build.c
@@ -3,61 +3,64 @@
 #define QOL_STRIP_PREFIX
 #include "libs/build.h"
 
+void base(Cmd* command) {
+    push(command, "cc");
+    push(command, "-O3");
+    push(command, "-Wall");
+    push(command, "-Wextra");
+    push(command, "-Wno-unused-function");
+    push(command, "-I./libs/raylib-5.5_macos/include");
+}
+
+Cmd cmd = {0};
+Procs procs = {0};
+
 int main() {
     auto_rebuild(__FILE__);
 
-    const char *files[][2] = {
-        { "main.c",                     "out/main.o" },
-        { "entries/maze.c",             "out/maze.o" },
-        { "entries/sort.c",             "out/sort.o" },
-        { "entries/plotter.c",          "out/plotter.o" },
-        { "entries/hexdump.c",          "out/hexdump.o" },
-        { "algorithms/maze/common.c",   "out/common.o" },
-        { "algorithms/maze/bfs.c",      "out/bfs.o" },
-        { "algorithms/maze/dfs.c",      "out/dfs.o" },
-        { "algorithms/maze/dijkstra.c", "out/dijkstra.o" },
-        { "algorithms/maze/astar.c",    "out/astar.o" },
-        { "algorithms/maze/greedy.c",   "out/greedy.o" },
-        { "algorithms/sort/heap.c",     "out/heap.o" },
-        { "algorithms/sort/merge.c",    "out/merge.o" },
-        { "algorithms/sort/quick.c",    "out/quick.o" },
-        { "algorithms/sort/selection.c","out/selection.o" },
-        { "algorithms/sort/bubble.c",   "out/bubble.o" },
-    };
+    const char* src_folders[] = {"entries", "algorithms/maze", "algorithms/sort"};
+    const char* out_folder = "out";
+    list(const char*) files = {0};
 
-    mkdir_if_not_exists("out/");
+    mkdir_if_not_exists(out_folder);
 
-    for (size_t i = 0; i < ARRAY_LEN(files); i++) {
-        Cmd cc = (Cmd){0};
-        push(&cc, "cc");
-        push(&cc, "-O3");
-        push(&cc, "-Wall");
-        push(&cc, "-Wextra");
-        push(&cc, "-Wno-unused-function");
-        push(&cc, "-I./libs/raylib-5.5_macos/include");
-        push(&cc, "-c");
-        push(&cc, files[i][0]);
-        push(&cc, "-o");
-        push(&cc, files[i][1]);
-        if (!run_always(&cc)) return EXIT_FAILURE;
+    // Compile the entries/*.c and algorithms/*/*.c source files
+    for (size_t sf = 0; sf < ARRAY_LEN(src_folders); sf++) {
+        const char* src_folder = src_folders[sf];
+        String contents = {0};
+
+        if (!read_dir(src_folder, &contents)) return EXIT_FAILURE;
+
+        for (size_t i = 0; i < contents.len; i++) {
+            const char* src_path = contents.data[i];
+            
+            if (!str_ends_with(src_path, ".c")) continue;
+            const char* temp = str_replace(src_path, ".c", ".o");
+            const char* new_path = str_replace(temp, src_folder, out_folder);
+            if (!temp || !new_path) continue;
+
+            base(&cmd);
+            push(&cmd, "-c", src_path, "-o", new_path);
+            if (!run(&cmd)) return EXIT_SUCCESS;
+            push(&files, new_path);
+        }
     }
 
+    // Compile the main.c
+    base(&cmd);
+    push(&cmd, "-c", "main.c", "-o", "out/main.o");
+    if (!run(&cmd)) return EXIT_SUCCESS;
+    push(&files, "out/main.o");
+
+    // Link all object files into the final executable
     Cmd link = (Cmd){0};
-    push(&link, "cc");
-    push(&link, "-O3");
-    push(&link, "-Wall");
-    push(&link, "-Wextra");
-    push(&link, "-I./libs/raylib-5.5_macos/include");
+    base(&link);
     push(&link, "-L./libs/raylib-5.5_macos/lib");
     push(&link, "-Wl,-rpath,@executable_path/libs/raylib-5.5_macos/lib");
-    push(&link, "-lraylib");
-    push(&link, "-lm");
-    push(&link, "-o");
-    push(&link, "main");
-    for (size_t i = 0; i < ARRAY_LEN(files); i++) {
-        push(&link, files[i][1]);
-    }
-
+    push(&link, "-lraylib", "-lm");
+    push(&link, "-o", "main");
+    for (size_t i = 0; i < files.len; i++) push(&link, files.data[i]);
+    
     if (!run_always(&link)) return EXIT_FAILURE;
 
     return EXIT_SUCCESS;
